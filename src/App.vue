fed481ae6f45cb12814bf45ce4996a9d80e29937
Léni Vienne <leni.vienne@developpement-durable.gouv.fr>
2025-04-13 22:55:34 +0200

    using indexedDB + fix bug in marker position

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
       </div>
       <div class="card flex justify-center">
         <div class="w-56">
-          <Button @click="clearLocalStorage">Clear Local Storage</Button>
+          <Button @click="clearIndexedDB">Clear Local Storage</Button>
         </div>
       </div>
     </div>
@@ -30,14 +30,16 @@ import 'leaflet-toolbar/dist/leaflet.toolbar.css';
 import "leaflet-distortableimage-updated/dist/leaflet.distortableimage.css";
 import './assets/style.css' // must be imported otherwise it's overwritten by leaflet's default css
 import { onMounted, ref, onUnmounted, shallowRef } from 'vue';
+import { openDB } from 'idb'; // Import the idb library
 
 interface overlayObject {
   id: string;
   overlay: L.ImageOverlay;
-  marker: L.Marker; // Add marker property
+  marker: L.Marker;
   history: { lat: number, lng: number }[][];
   redoStack: { lat: number, lng: number }[][];
-  isNewImage: boolean;
+  alreadyLoaded: boolean; // Track if the overlay is already loaded in the page
+  alreadyStored: boolean; // Track if the overlay is already stored in IndexedDB
 }
 
 const map = shallowRef<L.Map | null>(null); // shallowRef is used to avoid reactivity issues with Leaflet, see https://stackoverflow.com/a/73588115/12498040
@@ -45,15 +47,47 @@ const overlays = shallowRef<Record<string, overlayObject>>({}); // Store overlay
 const idSelectedOverlay = ref<string | null>(null); // Track the ID of the currently selected overlay
 const imageUrl = ref<string | null>(null);
 const isEditMode = ref<boolean>(true);
+let db: IDBDatabase | null = null;
 
 onMounted(async () => {
-  const savedPosition = localStorage.getItem('mapPosition');
-  const initialView = savedPosition ? JSON.parse(savedPosition) : { center: [48.845, 2.424], zoom: 10 };
+  await initializeDatabase();
+  await initializeMap();
+  window.addEventListener('keydown', handleKeyDown); // to handle undo/redo with ctrl+z and ctrl+y
+  await getOverlaysFromIndexedDB();
+});
+
+async function initializeDatabase() {
+  db = await openDB('CityMapOverlayDB', 1, {
+    upgrade(upgradeDb) {
+      if (!upgradeDb.objectStoreNames.contains('overlays')) {
+        upgradeDb.createObjectStore('overlays', { keyPath: 'id' });
+      }
+      if (!upgradeDb.objectStoreNames.contains('mapPosition')) {
+        upgradeDb.createObjectStore('mapPosition', { keyPath: 'key' });
+      }
+    },
+  });
+}
+
+async function initializeMap() {
+  const savedPosition = await getSavedMapPosition();
+  const initialView = savedPosition || { center: [48.845, 2.424], zoom: 10 };
 
   map.value = L.map("viewerDiv").setView(initialView.center, initialView.zoom);
-
   if (!map.value) throw new Error('No map element found');
 
+  addTileLayer();
+  map.value.on('moveend', saveMapPosition);
+  map.value.on('zoomend', saveMapPosition);
+}
+
+async function getSavedMapPosition() {
+  if (!db) return null;
+  const savedPosition = await db.get('mapPosition', 'position');
+  return savedPosition ? savedPosition.value : null;
+}
+
+function addTileLayer() {
   L.tileLayer(
     'https://data.geopf.fr/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=PM&tilematrix={z}&tilecol={x}&tilerow={y}&layer=ORTHOIMAGERY.ORTHOPHOTOS&format=image/jpeg&style=normal',
     {
@@ -63,35 +97,36 @@ onMounted(async () => {
       attribution: "IGN-F/Géoportail"
     }
   ).addTo(map.value);
+}
 
-  map.value.on('moveend', saveMapPosition);
-  map.value.on('zoomend', saveMapPosition);
+async function getOverlaysFromIndexedDB() {
+  if (!db) return;
 
-  window.addEventListener('keydown', handleKeyDown);
-
-  await getOverlaysFromLocalStorage();
-});
-
-async function getOverlaysFromLocalStorage() {
-  const savedOverlays = localStorage.getItem('overlays');
-  if (!savedOverlays) return;
-
-  const parsedOverlays = JSON.parse(savedOverlays);
-  parsedOverlays.forEach(async (savedOverlay: { id: string, imageUrl: string, corners: { lat: number, lng: number }[] }) => {
-    const overlayObject = {
-      id: savedOverlay.id,
-      overlay: null as L.ImageOverlay,
-      marker: null as L.Marker,
-      history: [savedOverlay.corners], // Initialize history with saved corners
-      redoStack: [],
-      isNewImage: false, // Set to false since it's loaded from storage
-    };
+  const savedOverlays = await fetchSavedOverlays();
+  savedOverlays.forEach(async (savedOverlay) => {
+    const overlayObject = createOverlayObject(savedOverlay);
     const newOverlay = await createOverlay(savedOverlay.imageUrl, overlayObject);
-
-    overlayObject.overlay = newOverlay!; // Set the overlay property to the created overlay
-    
+    overlayObject.overlay = newOverlay!;
     overlays.value[savedOverlay.id] = overlayObject;
   });
+}
+
+async function fetchSavedOverlays() {
+  const transaction = db.transaction('overlays', 'readonly');
+  const store = transaction.objectStore('overlays');
+  return await store.getAll();
+}
+
+function createOverlayObject(savedOverlay: { id: string, imageUrl: string, corners: { lat: number, lng: number }[] }) {
+  return {
+    id: savedOverlay.id,
+    overlay: null as L.ImageOverlay,
+    marker: null as L.Marker,
+    history: [savedOverlay.corners],
+    redoStack: [],
+    alreadyLoaded: false,
+    alreadyStored: true
+  };
 }
 
 function handleKeyDown(event: KeyboardEvent) {
@@ -104,14 +139,14 @@ function handleKeyDown(event: KeyboardEvent) {
 
 async function onImageUpload(event: Event) {
   const file = (event.target as HTMLInputElement).files?.[0];
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = async () => {
-      imageUrl.value = reader.result as string; // Store base64 string
-      await addOverlay();
-    };
-    reader.readAsDataURL(file); // Convert file to base64
-  }
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.onload = async () => {
+    imageUrl.value = reader.result as string; // Store base64 string
+    await addOverlay();
+  };
+  reader.readAsDataURL(file); // Convert file to base64
 }
 
 const CustomAction = L.Toolbar2.Action.extend({
@@ -127,42 +162,43 @@ const CustomAction = L.Toolbar2.Action.extend({
 });
 
 async function createOverlay(imageUrl: string, overlayObject?: overlayObject) {
-  if (!map.value) return null;
+  if (!map.value || !overlayObject) return null;
 
   const newOverlay = L.distortableImageOverlay(imageUrl, {
     editable: isEditMode.value,
   }).addTo(map.value);
 
-  if (overlayObject) {
-    overlayObject.overlay = newOverlay;
+  overlayObject.overlay = newOverlay;
 
-    newOverlay.on('edit', () => {
-      saveToHistory(overlayObject);
-      saveImageAndPosition();
-      updateMarkerPosition(overlayObject);
-    });
-    newOverlay.on('dragend', () => {
-      saveToHistory(overlayObject);
-      saveImageAndPosition();
-      updateMarkerPosition(overlayObject);
-    });
-    newOverlay.on('select', () => {
-      idSelectedOverlay.value = overlayObject.id;
-    });
+  newOverlay.on('edit', () => {
+    saveToHistory(overlayObject);
+    saveImageAndPosition();
+    updateMarkerPosition(overlayObject);
+  });
+  newOverlay.on('dragend', () => {
+    saveToHistory(overlayObject);
+    saveImageAndPosition();
+    updateMarkerPosition(overlayObject);
+  });
+  newOverlay.on('select', () => {
+    idSelectedOverlay.value = overlayObject.id;
+  });
 
-    //allows to access corners of the image on load since newOverlay.on('load') doesn't work, credit to https://github.com/publiclab/Leaflet.DistortableImage/issues/953#issuecomment-1262298228
-    L.DomEvent.on(newOverlay.getElement(), 'load', () => {
-      if (overlayObject.isNewImage) {
-        saveToHistory(overlayObject);
-        overlayObject.marker = createMarker(overlayObject);
-      } else {
-        // for the modified image to keep the same position as before
-        overlayObject.overlay.setCorners(overlayObject.history.at(-1));
-      }
-      // isNewImage must be updated there otherwise it is set to false too early
-      overlayObject.isNewImage = false
-    });
-  }
+  // allows to access the corners of the image on load since newOverlay.on('load') doesn't work
+  // credit to https://github.com/publiclab/Leaflet.DistortableImage/issues/953#issuecomment-1262298228
+  L.DomEvent.on(newOverlay.getElement(), 'load', () => {
+    if (overlayObject.alreadyStored || overlayObject.alreadyLoaded) {
+      overlayObject.overlay.setCorners(overlayObject.history.at(-1));
+    }
+
+    if (!overlayObject.alreadyLoaded) {
+      saveToHistory(overlayObject);
+      overlayObject.marker = createMarker(overlayObject);
+    }
+
+    overlayObject.alreadyLoaded = true
+    overlayObject.alreadyStored = true
+  });
   return newOverlay;
 }
 
@@ -179,23 +215,38 @@ function updateMarkerPosition(overlayObject: overlayObject) {
   overlayObject.marker.setLatLng(center);
 }
 
-function saveMapPosition() {
-  if (map.value) {
-    const center = map.value.getCenter();
-    const zoom = map.value.getZoom();
-    localStorage.setItem('mapPosition', JSON.stringify({ center: [center.lat, center.lng], zoom }));
-  }
+async function saveMapPosition() {
+  if (!map.value || !db) return;
+
+  const center = map.value.getCenter();
+  const zoom = map.value.getZoom();
+  const position = { key: 'position', value: { center: [center.lat, center.lng], zoom } };
+
+  const transaction = db.transaction('mapPosition', 'readwrite');
+  const store = transaction.objectStore('mapPosition');
+  await store.put(position);
 }
 
 async function saveImageAndPosition() {
+  if (!db) return;
+
   const savedOverlays = Object.values(overlays.value).map(overlayObject => ({
     id: overlayObject.id,
-    imageUrl: overlayObject.overlay.getElement().src, // Save only the image URL
-    corners: overlayObject.overlay.getCorners(), // Save only the corners
-    history: overlayObject.history, // Save history for undo/redo
-    redoStack: overlayObject.redoStack, // Save redo stack for undo/redo
+    imageUrl: overlayObject.overlay.getElement().src,
+    corners: overlayObject.overlay.getCorners(),
+    history: overlayObject.history,
+    redoStack: overlayObject.redoStack,
   }));
-  localStorage.setItem('overlays', JSON.stringify(savedOverlays));
+
+  await saveOverlaysToDB(savedOverlays);
+}
+
+async function saveOverlaysToDB(overlays: { id: string, imageUrl: string, corners: { lat: number, lng: number }[], history: { lat: number, lng: number }[][], redoStack: { lat: number, lng: number }[][] }[]) {
+  const transaction = db.transaction('overlays', 'readwrite');
+  const store = transaction.objectStore('overlays');
+  for (const overlay of overlays) {
+    await store.put(overlay);
+  }
 }
 
 function saveToHistory(overlayObject: { id: string, overlay: L.ImageOverlay, history: { lat: number, lng: number }[][], redoStack: { lat: number, lng: number }[][] }) {
@@ -214,7 +265,8 @@ async function addOverlay() {
     marker: null as L.Marker,
     history: [],
     redoStack: [],
-    isNewImage: true
+    alreadyLoaded: false,
+    alreadyStored: false // Set to false since it's a new overlay
   };
 
   const newOverlay = await createOverlay(imageUrl.value, overlayObject);
@@ -237,6 +289,8 @@ function undo() {
 
   const previousState = history[history.length - 1];
   overlay.setCorners(previousState);
+
+  updateMarkerPosition(overlayObject);
 }
 
 function redo() {
@@ -252,18 +306,21 @@ function redo() {
   history.push(nextState);
 
   overlay.setCorners(nextState);
+
+  updateMarkerPosition(overlayObject);
 }
 
 function toggleEditMode() {
   isEditMode.value = !isEditMode.value;
+  Object.values(overlays.value).forEach(toggleOverlayEditMode);
+}
 
-  Object.values(overlays.value).forEach(overlayObject => {
-    if (isEditMode.value) {
-      overlayObject.overlay.editing.enable(); // editable = true doesn't work
-    } else {
-      overlayObject.overlay.editing.disable();
-    }
-  });
+function toggleOverlayEditMode(overlayObject: overlayObject) {
+  if (isEditMode.value) {
+    overlayObject.overlay.editing.enable();
+  } else {
+    overlayObject.overlay.editing.disable();
+  }
 }
 
 async function removeWhitePixels() {
@@ -308,9 +365,18 @@ async function removeWhitePixels() {
   img.src = overlay.getElement().src; // what is actually updating the image
 }
 
-function clearLocalStorage() {
-  localStorage.clear();
-  alert('Local storage cleared!');
+function clearIndexedDB() {
+  if (!db) return;
+
+  const transactionOverlays = db.transaction('overlays', 'readwrite');
+  const storeOverlays = transactionOverlays.objectStore('overlays');
+  storeOverlays.clear();
+
+  const transactionMapPosition = db.transaction('mapPosition', 'readwrite');
+  const storeMapPosition = transactionMapPosition.objectStore('mapPosition');
+  storeMapPosition.clear();
+
+  alert('IndexedDB cleared!');
 }
 
 onUnmounted(() => {
