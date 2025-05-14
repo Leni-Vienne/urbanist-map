ece6fe1abf5439d6e21a10e92e25585cf5f56ab6
Léni Vienne <leni.vienne@developpement-durable.gouv.fr>
2025-05-14 21:39:36 +0200

    added new leaflet tools;

--- src/types.ts
+++ src/types.ts
@@ -19,6 +19,8 @@ declare module "leaflet" {
   const StackAction: any;
   const Toolbar2: any;
   const EditAction: any;
+  const DragAction: any;
+  const ResizeRotateAction: any;
 
   // AI : Definition for DistortableImageOverlay
   interface DistortableImageOverlay extends L.ImageOverlay {
