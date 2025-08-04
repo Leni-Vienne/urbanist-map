2a75f3ff1d6b366102361e1ccda9969aca7399e7
Léni Vienne <leni.vienne@developpement-durable.gouv.fr>
2025-08-04 23:02:04 +0200

    testing with ssl rejectUnauthorized set to false

--- back/drizzle.config.ts
+++ back/drizzle.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
     extensionsFilters: ['postgis'], // To prevent drizzle migrations from trying to delete 'spatial_ref_sys' table
     dbCredentials: {
         url: process.env.DATABASE_URL!,
-        ssl: 'require'
+        ssl: {
+            rejectUnauthorized: false,
+        }
     },
 });
