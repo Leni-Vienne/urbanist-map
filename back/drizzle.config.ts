22d8dfcf7cdb734d19956d0ab4f8ab4a89e00d4c
Léni Vienne <leni.vienne@developpement-durable.gouv.fr>
2025-08-04 22:48:06 +0200

    adding ssl param to drizzle.config.ts

--- back/drizzle.config.ts
+++ back/drizzle.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
     extensionsFilters: ['postgis'], // To prevent drizzle migrations from trying to delete 'spatial_ref_sys' table
     dbCredentials: {
         url: process.env.DATABASE_URL!,
+        ssl: 'require'
     },
 });
