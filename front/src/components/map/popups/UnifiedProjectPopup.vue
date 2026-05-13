f38e14261c0a55830a2199def09fe851b6ad4dfd
Leni_Vienne <leni.vienne@test.com>
2026-05-13 13:36:40 +0200

    fixed not being able to scroll a projectPopup on mobile

--- front/src/components/map/popups/UnifiedProjectPopup.vue
+++ front/src/components/map/popups/UnifiedProjectPopup.vue
@@ -8,6 +8,9 @@
     class="w-max min-w-60 max-w-80 min-h-50 bg-content-background cursor-text select-text rounded-2xl shadow-[0_12px_40px_rgba(0,0,0,0.14),0_2px_6px_rgba(0,0,0,0.06)] pointer-events-auto relative z-1000"
     @click.stop
     @mousedown.stop
+    @touchstart.stop
+    @touchmove.stop
+    @touchend.stop
   >
     <div v-if="loading" class="flex justify-center items-center h-50 p-4">
       <i class="pi pi-spin pi-spinner"></i>
@@ -115,7 +118,7 @@
       </div>
 
       <!-- Project fields + overlay section, wheel.stop prevents map zoom while scrolling -->
-      <div class="px-4 pt-3 pb-4 overflow-y-auto" @wheel.stop>
+      <div class="px-4 pt-3 pb-4 overflow-y-auto touch-pan-y overscroll-contain" @wheel.stop>
         <ProjectMetadataCard
           :project="project"
           :show-name="false"
